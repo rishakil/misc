ddb82c885e44d040665ffd955f70209c7cea92ab
Rokibul Islam <69357501+rishakil@users.noreply.github.com>
2023-10-05 03:25:54 +0600

    Update ris.cpp

--- ris.cpp
+++ ris.cpp
@@ -13,7 +13,7 @@ ll _digit(ll x) { ll cnt = 0; while (x > 0) { x /= 10; cnt++; } return cnt; }
 bool is_prime(ll x) { if(x==2) return true; else {bool flag = false; for(ll i = 2; i*i<=x; i++) {if(x%i==0) { flag = true; break; }} return flag ? false : true; }}
 long long GCD(long long x, long long y) { if (y == 0) return x; return GCD(y, x%y); }
 bool isUp(char c) {if(c>='A' && c<='Z') return true; else return false;}
-
+bool isPowerOfTwo (int x) {return x && (!(x&(x-1)));}
 int main() {
 	return 0;
 }
